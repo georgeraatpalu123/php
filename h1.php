b5012e5a6511d4e43c73ebbe1beac9e78d27b19d
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2024-02-05 13:47:51 +0200

    h1.php

---
+++ h1.php
@@ -0,0 +1,13 @@
+<?php
+
+	$enimi = 'George';
+	$pnimi = "Raatpalu";
+	$nimi = $enimi.' '.$pnimi;
+	$vanus = 17;
+	$kaal = 86;
+	
+	echo 'Tere '.$nimi.'!<br>';
+	echo 'Sinu vanus: '.$vanus.'<br>';
+	echo 'Sinu kaal: '.$kaal.'<br>';
+    echo "“It’s My Life” – Dr. Alban"
+?>
