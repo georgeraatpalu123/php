572fbd613962d9637381c3d921a6dd84289db75e
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2024-02-07 15:31:33 +0200

    Update h4.php

--- h4.php
+++ h4.php
@@ -30,45 +30,39 @@
                 $s=$a/$b;
                 echo "Jagatis on ".$s."<br>";
             }elseif($_GET["arv1"] == 0 or $_GET["arv2"] == 0 ){echo "Sisesta midagi õiget";}
+        
             ?>
-        
-        
-        
 
-    <div class="form-group">
-            <label for="arv2">Esimene vanus:</label>
-            <input type="number" class="form-control" name="arv1" required>
-        </div>
+
 
         <div class="form-group">
-            <label for="arv2">Teine vanus vanus:</label>
-            <input type="number" class="form-control" name="arv2" required>
-            <br>
-            <button type="submit" class="btn btn-danger">Arvuta vanus</button>
+            <form action="" method="get">
+                <label for="y">Sisesta Esimene vanus</label>
+                <input type="number" class="form-control" name="y" id="y"><br>
+                <label for="x">Sisesta teine vanus</label>
+                <input type="number" class="form-control" name="x" id="x"><br>
+                <input type="submit" class="btn btn-danger my-2" value="Arvuta">
+            </form>
+            <?php
     
-        
-        
-        <div class="form-group">
-            <label for="kylg1">Esimese külje pikkus</label>
-            <input type="number" class="form-control" name="arv1" required>
-        </div>
+        if(!empty($_GET["x"]) && !empty($_GET["y"])) {
+            $x = intval($_GET["y"]);
+            $y = intval($_GET["x"]);
+            if($x > $y) {
+                echo "Esimene vanus ($x) on vanem kui teine  ($y).";
+            } elseif($y < $x) {
+                echo "Teine vanus ($y) on vanem kui esimene vanus ($x).";
+            } else {
+             echo "Mõlemad on ühevanused ($x).";
+            }
+            } else {
+                echo "Palun täitke mõlemad vanuse lahtrid.";
+            }
 
-        <div class="form-group">
-            <label for="kylg2">Teine külje pikkus</label>
-            <input type="number" class="form-control" name="arv1" required>
-        </div>
-    
-        <button type="submit" class="btn btn-primary">Arvuta küljed</button>
-        </form>
-    
-    
-   
-    
+        ?>
 
     
     
-        
-   
 
 
 
@@ -76,8 +70,11 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
