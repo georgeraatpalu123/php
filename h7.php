b84d6a1625bad34933df92cf60c4d3cde8a77994
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2024-02-19 15:33:20 +0200

    Update h7.php

--- h7.php
+++ h7.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>h6</title>
+    <title>h7</title>
 </head>
 <body>
     
@@ -12,13 +12,6 @@
 
 <?php
 echo "Tervitus<br>";
-
-
-
-
-
-
-
 
 
 
@@ -41,13 +34,70 @@ function vaike($nimi) {
 }
 
 
-$nimi = "GeoERGE";
+$nimi = "George";
 echo vaike($nimi);
 
 
 
+function generateEmailAndCode($nimi) {
+   
+    $nimi = strtolower($nimi);
+
+  
+    $email=$nimi . "@hkhk.edu.ee";
+
+
+    $tähed = "abcdefghijklmnopqrstuvwxyz0123456789";
+    $code = "";
+    $codeLength = 7;
+    $tähepikkus = strlen($tähed);
+    for ($i = 0; $i < $codeLength; $i++) {
+        $code .= $tähed[rand(0, $tähepikkus - 1)];
+    }
+
+    return array("email" => $email, "code" => $code);
+}
+
+
+$nimi = "georgeraatpalu";
+
+
+$result = generateEmailAndCode($nimi);
+
+echo "gmail: " . $result["email"] . "<br>";
+echo "kood: " . $result["code"];
+
+function suvakadnumbrid($start, $end) {
+    $numbrid = [];
+    for ($i = $start; $i <= $end; $i++) {
+        $numbrid[] = $i;
+    }
+    return $numbrid;
+}
+
+$start = 2;
+$end = 8;
+echo implode(", ", suvakadnumbrid($start, $end));
+
+function ristkulik($pikkus,$laius)  {
+ $tehe=$pikkus * $laius;
+ return $tehe;
+}
+ $fpikkus = 5;
+ $flaius = 8;
+
+ $tehe=ristkulik($fpikkus,$flaius);
+
+echo "<br> ristkulik on : " . $tehe;
+
+
+$isikukood= "<br> 37505280379";
+echo $isikukood
 
 ?>
+
+
+
 
 
 
