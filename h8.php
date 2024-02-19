0f66bb7f00b9620ec1008c8c30a395061f8cb3b6
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2024-02-19 15:33:47 +0200

    Create h8.php

---
+++ h8.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>h8</title>
+</head>
+<body>
+ 
+<?php
+$sp = mktime(0,0,0,20,03,2023);
+echo date('d.m.Y', $sp); 
+
+
+
+?>
+
+
+
+</body>
+</html>
