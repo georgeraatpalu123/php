b80a95b6e467f80429c5a4a634522ded2978768d
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2024-02-23 14:01:18 +0200

    Update h8.php

--- h8.php
+++ h8.php
@@ -12,8 +12,42 @@ $sp = mktime(0,0,0,20,03,2023);
 echo date('d.m.Y', $sp); 
 
 
+function arvutus($synd) {
+    $aasta = date("Y");
+    $synd = date("Y", strtotime($synd));
+    $vanus = $aasta - $synd;
+    return $vanus;
+}
+
+
+$synniaasta = "2023-10-02";
+
+
+$vanus = arvutus($synniaasta);
+echo "<br> kasutaja vanus sellel aastal on:  " . $vanus. " aastat.";
+
+
+
+
+
+$kooliaasta = strtotime("23 february 2023");
+
+
+$paevad = ceil(($kooliaasta  - time()) / (60 * 60 * 24));
+
+
+echo " <br> kooliaastani on jaand $paevad paeva";
+
+
+
 
 ?>
+
+
+
+
+
+
 
 
 
